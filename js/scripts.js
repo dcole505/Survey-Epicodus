921af505dc9c4c2dbab36256a2c5048891c28238
Michael Watts <wattsjmichael@gmail.com>
2020-08-12 15:26:25 -0700

    add functionality for radio, dropdown buttons
    Co:authored-By: Amanda Otero <Mandystar018@gmail.com>
    co-authored-by: David Cole <davidtcole@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
 $("form#name").submit(function(event){
   event.preventDefault();
-  const firstName= $("input#firstName").val();
-  const lastName= $("input#lastName").val();
+  const firstName = $("input#firstName").val();
+  const lastName = $("input#lastName").val();
+  const favFood = $("#food").val();
+  const favAnimal = $("input:radio[name=animal]:checked").val();
 
 
   $(".firstName").text(firstName);
   $(".lastName").text(lastName);
+  $(".favoriteFood").text(favFood);
+  $(".favoriteAnimal").text(favAnimal);
 
   $("#favoriteThings").show();
   });
